c80dc6ebe9b3622a55fb094f49c271d5a71b6c5b
Sushant Kumar <sushant.kum96@gmail.com>
2019-07-23 16:10:14 +0530

    card-link component converted to module

---
+++ src/app/components/card-link/card-link.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardLinkComponent } from './card-link.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    CardLinkComponent
+  ],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [
+    CardLinkComponent
+  ]
+})
+export class CardLinkModule { }
