983ae7c11535de95407edfecb5f01edd93027ddb
Sushant Kumar <sushant.kum96@gmail.com>
2019-07-29 18:48:39 +0530

    ...

--- src/app/pages/settings/profile/profile.module.ts
+++ src/app/pages/settings/profile/profile.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 /* Angular Material Imports */
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 /* Fontawesome Imports */
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 /* Solid Icons */
-import {} from '@fortawesome/free-solid-svg-icons';
-library.add();
+import {
+  faUser as fasUser,
+  faUserShield as fasUserShield
+} from '@fortawesome/free-solid-svg-icons';
+library.add(fasUser, fasUserShield);
 /* Regular Icons */
-import {} from '@fortawesome/free-regular-svg-icons';
+import { } from '@fortawesome/free-regular-svg-icons';
 library.add();
 /* Brand Icons */
-import {} from '@fortawesome/free-brands-svg-icons';
+import { } from '@fortawesome/free-brands-svg-icons';
 library.add();
 
 import { ProfileRoutingModule } from './profile-routing.module';
@@ -25,13 +31,16 @@ import { ProfileComponent } from './profile.component';
   imports: [
     /* Angular Material Imports */
     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
 
     /* Font-awesome Impors */
     FontAwesomeModule,
 
     /* Other Imports */
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProfileModule { }
