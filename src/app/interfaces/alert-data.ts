990c9248b5801120c0c8a0d7eec17ac60b13cc43
Sushant Kumar <sushant.kum96@gmail.com>
2019-08-17 22:59:05 +0530

    ...

--- src/app/interfaces/alert-data.ts
+++ src/app/interfaces/alert-data.ts
@@ -1,6 +1,6 @@
 export interface AlertData {
-    title?: string;
-    message: string;
-    info?: string;
-    positive_btn_text?: string;
+  title?: string;
+  message: string;
+  info?: string;
+  positive_btn_text?: string;
 }
