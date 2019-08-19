1cda8f3f3d5f4172f0407b5198985b9681de074c
Sushant Kumar <sushant.kum96@gmail.com>
2019-08-19 18:45:03 +0530

    errors/error404

--- src/app/errors/error404/error404.component.spec.ts
+++ src/app/errors/error404/error404.component.spec.ts
@@ -8,9 +8,9 @@ describe('Error404Component', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ Error404Component ]
+      declarations: [Error404Component]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
