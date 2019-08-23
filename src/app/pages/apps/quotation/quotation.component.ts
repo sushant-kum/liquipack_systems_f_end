cdbb2893bc769829663ae5eb12b0e24a9062a3c1
Sushant Kumar <sushant.kum96@gmail.com>
2019-08-23 18:16:29 +0530

    ...

--- src/app/pages/apps/quotation/quotation.component.ts
+++ src/app/pages/apps/quotation/quotation.component.ts
@@ -14,10 +14,16 @@ import { HeaderService } from 'src/app/services/header/header.service';
 import { HttpTransactionsService } from 'src/app/services/http-transactions/http-transactions.service';
 import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { HelperService } from 'src/app/services/helper/helper.service';
+import { ConfirmService } from 'src/app/services/confirm/confirm.service';
 
 /* Config Imports */
 import { Config } from 'src/app/configs/config';
-import { RegexService } from 'src/app/services/regex/regex.service';
+
+/* Interface Imports */
+import { QuotationData } from 'src/app/interfaces/quotation-data';
+import { DialogResponse } from 'src/app/interfaces/dialog-response';
+import { ApiResponse } from 'src/app/interfaces/api-response';
 
 interface Mode {}
 
@@ -28,13 +34,17 @@ const PAGE_ID = 'apps-quotation';
   templateUrl: './quotation.component.html',
   styleUrls: ['./quotation.component.scss']
 })
-export class QuotationComponent implements OnInit {
+export class QuotationComponent implements OnInit, OnDestroy {
   private _page_id = PAGE_ID;
   config: Config = new Config();
 
   private _auth_state_change_subscription: Subscription;
 
   mode: Mode = {};
+
+  app_permission: ('read' | 'write')[] = [];
+
+  quotations: QuotationData[] = [];
 
   constructor(
     private _title: Title,
@@ -44,7 +54,7 @@ export class QuotationComponent implements OnInit {
     private _http_service: HttpTransactionsService,
     private _localstorage_service: LocalStorageService,
     private _auth_service: AuthService,
-    private _regexp_sevice: RegexService
+    public helper_service: HelperService
   ) { }
 
   ngOnInit() {
@@ -57,10 +67,74 @@ export class QuotationComponent implements OnInit {
 
     this._sidebar.activate();
     this._sidebar.colorize(this.config.page_map[this._page_id].identifier);
+
+    this._auth_state_change_subscription = this._auth_service.auth_state_change.subscribe(
+      (auth_state: boolean) => {
+        if (auth_state) {
+          this.getQuotations();
+          const app_permissions = JSON.parse(this._localstorage_service.get(this._localstorage_service.lsname.app_permissions));
+          for (const app of app_permissions) {
+            if (app.app === this._page_id) {
+              this.app_permission = app.permissions;
+              break;
+            }
+          }
+          if (!this.app_permission.includes('read')) {
+            alert('You are not allowed here');
+            this._sidebar.logout();
+          }
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._auth_state_change_subscription.unsubscribe();
   }
 
   routeToConfig(): void {
     this._sidebar.redirectTo('/apps/quotation/config', null, true);
+  }
+
+  getQuotations(): void {
+    this._http_service.get_quotations.sendRequest().subscribe(
+      (res: ApiResponse) => {
+        for (const quotation of res.data) {
+          let total_price = 0;
+          for (const key of Object.keys(quotation)) {
+            if (quotation[key] && quotation[key].price) {
+              total_price += quotation[key].price;
+            }
+          }
+          this.quotations.push({
+            _id: quotation._id,
+            quotation_num: quotation.quotation_num,
+            speed: quotation.speed,
+            no_of_washes: quotation.no_of_washes,
+            industry: quotation.industry,
+            gmp_requirement: quotation.gmp_requirement,
+            bottle_moc: quotation.bottle_moc,
+            water_saving: quotation.water_saving,
+            filters_required: quotation.filters_required,
+            illumination_required: quotation.illumination_required,
+            auto_level_tank: quotation.auto_level_tank,
+            extra_cups_sets: quotation.extra_cups_sets,
+            other_details: quotation.other_details ? quotation.other_details : null,
+            customer_details: quotation.customer_details,
+            crated_by: quotation.crated_by,
+            created_date: quotation.created_date,
+            is_active: quotation.is_active,
+            extra_data: {
+              total_price
+            }
+          });
+        }
+      },
+      (err: Error) => {
+        console.log(err);
+        this.showToast('Something went wrong. Please try again later.', 'Close', null, true);
+      }
+    );
   }
 
 
