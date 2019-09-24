a1514b5222c844deffa54e2eafc0279fd9069fe0
Sushant Kumar <sushant.kum96@gmail.com>
2019-09-24 19:23:19 +0530

    add quotation-config

--- src/app/services/regex/regex.service.ts
+++ src/app/services/regex/regex.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class RegexService {
   private _phone: RegExp = /^[1-9][0-9]{9}$/;
   private _username: RegExp = /^(?=.{2,50}$)(?![_.])(?!.*[_.]{2})[a-z0-9._]+[a-z0-9]$/;
-  private _quotation_config_name: RegExp = /^(?=.{2,50}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+[a-z0-9]$/;
+  private _quotation_config_name: RegExp = /^(?=.{2,50}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+[a-zA-Z0-9]$/;
   private _price: RegExp = /^(\-?(\d+\.?\d{0,2}|\d*\.?\d{1,2}))?$/;
 
   constructor() {}
