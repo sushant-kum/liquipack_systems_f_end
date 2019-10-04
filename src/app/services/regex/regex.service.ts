1055b58a003fcab66bd9cf71ce30f1246a33426d
Sushant Kumar <sushant.kum96@gmail.com>
2019-10-04 11:07:25 +0530

    ...

--- src/app/services/regex/regex.service.ts
+++ src/app/services/regex/regex.service.ts
@@ -8,7 +8,7 @@ export class RegexService {
   private _username: RegExp = /^(?=.{2,50}$)(?![_.])(?!.*[_.]{2})[a-z0-9._]+[a-z0-9]$/;
   private _quotation_num: RegExp = /^(?=.{2,50}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+[a-zA-Z0-9]$/;
   private _quotation_config_name: RegExp = /^(?=.{2,50}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+[a-zA-Z0-9]$/;
-  private _price: RegExp = /^(\-?(\d+\.?\d{0,2}|\d*\.?\d{1,2}))?$/;
+  private _price: RegExp = /^(\-?(\d+\.?\d{0,9}|\d*\.?\d{1,9}))?$/;
 
   constructor() {}
 
