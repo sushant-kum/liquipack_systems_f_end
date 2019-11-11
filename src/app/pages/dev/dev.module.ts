8ed0257438519cd7ffeb33d858bcbf9c1361e876
Sushant Kumar <sushant.kum96@gmail.com>
2019-11-11 16:45:34 +0530

    app-palette - colors

--- src/app/pages/dev/dev.module.ts
+++ src/app/pages/dev/dev.module.ts
@@ -5,6 +5,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 /* Fontawesome Imports */
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
@@ -12,12 +13,13 @@ import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontaweso
 import {
   faFont as fasFont,
   faPalette as fasPalette,
-  faChevronRight as fasChevronRight
+  faChevronRight as fasChevronRight,
+  faSwatchbook as fasSwatchbook
 } from '@fortawesome/free-solid-svg-icons';
 /* Regular Icons */
 import {} from '@fortawesome/free-regular-svg-icons';
 /* Brand Icons */
-import { faCss3Alt as fabCss3Alt, faAngular as fasAngular } from '@fortawesome/free-brands-svg-icons';
+import { faAngular as fasAngular } from '@fortawesome/free-brands-svg-icons';
 
 import { DevRoutingModule } from './dev-routing.module';
 import { DevComponent } from './dev.component';
@@ -32,6 +34,7 @@ import { PaletteNgMaterialComponent } from './palette-ng-material/palette-ng-mat
     MatCardModule,
     MatButtonModule,
     MatListModule,
+    MatTooltipModule,
     FontAwesomeModule,
     CommonModule,
     DevRoutingModule
@@ -40,9 +43,9 @@ import { PaletteNgMaterialComponent } from './palette-ng-material/palette-ng-mat
 export class DevModule {
   constructor(fa_icon_library: FaIconLibrary) {
     // Include solid fa icons
-    fa_icon_library.addIcons(fasFont, fasPalette, fasChevronRight);
+    fa_icon_library.addIcons(fasFont, fasPalette, fasChevronRight, fasSwatchbook);
     // Include regular fa icons
     // Include brand fa icons
-    fa_icon_library.addIcons(fabCss3Alt, fasAngular);
+    fa_icon_library.addIcons(fasAngular);
   }
 }
--- src/app/pages/dev/palette-css/palette-css.component.ts
+++ src/app/pages/dev/palette-css/palette-css.component.ts
@@ -1,12 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import rgbHex from 'rgb-hex';
+
+/* Services Imports */
+import { HeaderService } from 'src/app/services/header/header.service';
+import { HelperService } from 'src/app/services/helper/helper.service';
+
+/* Config Imports */
+import { Config } from 'src/app/configs/config';
+
+interface ColorLevel {
+  suffix: string;
+  color_hex: string;
+  css_classes: string;
+}
+
+const COLOR_LEVEL_SUFFIXES = ['-l5', '-l4', '-l3', '-l2', '-l1', '', '-d1', '-d2', '-d3', '-d4'];
 
 @Component({
   selector: 'app-palette-css',
   templateUrl: './palette-css.component.html',
   styleUrls: ['./palette-css.component.scss']
 })
-export class PaletteCssComponent implements OnInit {
-  constructor() {}
+export class PaletteCssComponent implements OnInit, AfterViewChecked {
+  config: Config = new Config();
 
-  ngOnInit() {}
+  readonly palettes = [
+    { id: 'primary', name: 'Primary' },
+    { id: 'accent', name: 'Accent' },
+    { id: 'warn', name: 'Warn' }
+  ];
+  readonly color_levels: {
+    primary: ColorLevel[];
+    accent: ColorLevel[];
+    warn: ColorLevel[];
+  } = {
+    primary: [],
+    accent: [],
+    warn: []
+  };
+
+  constructor(public helper_service: HelperService, private _title: Title, private _header_service: HeaderService) {}
+
+  ngOnInit() {
+    this._title.setTitle('App Palette - Developers Page - ' + this.config.app_title);
+    this._header_service.changePageInfo('palette-app', 'App Palette', 'swatchbook');
+
+    for (const color_suffix of COLOR_LEVEL_SUFFIXES) {
+      for (const palette of this.palettes) {
+        let css_class = `.app-theme-${palette.id}${color_suffix}`;
+        if (COLOR_LEVEL_SUFFIXES.indexOf(color_suffix) === 0) {
+          css_class += `, .app-theme-${palette.id}-light`;
+        }
+        if (COLOR_LEVEL_SUFFIXES.indexOf(color_suffix) === COLOR_LEVEL_SUFFIXES.length - 1) {
+          css_class += `, .app-theme-${palette.id}-dark, .app-theme-${palette.id}-action`;
+        }
+
+        this.color_levels[palette.id].push({
+          suffix: color_suffix,
+          color_hex: '',
+          css_classes: css_class
+        });
+      }
+    }
+  }
+
+  ngAfterViewChecked() {
+    for (const palette of this.palettes) {
+      for (const color_level of this.color_levels[palette.id]) {
+        const ele: HTMLElement = document.querySelectorAll(
+          `.color-swatch.app-theme-${palette.id}${color_level.suffix}`
+        )[0] as HTMLElement;
+        color_level.color_hex = rgbHex(getComputedStyle(ele).getPropertyValue('background-color')).toUpperCase();
+      }
+    }
+  }
 }
